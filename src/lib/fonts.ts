257bdfecba4ae6d5a39ed093710d60010184cfa8
Oriz Wahyu N. <oriezt.id@gmail.com>
2025-06-22 12:31:40 +0700

    init

---
+++ src/lib/fonts.ts
@@ -0,0 +1,6 @@
+import { Comfortaa } from "next/font/google";
+
+export const comfortaa = Comfortaa({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" prefix="og: https://ogp.me/ns#">
       <Head />
-      <body>
+      <body className="text-sm antialiased bg-white">
         <Main />
         <NextScript />
       </body>
